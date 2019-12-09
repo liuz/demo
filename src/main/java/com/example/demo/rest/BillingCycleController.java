63792230f6d696660f4bb56eb666b413f2cf54c1
Peter <liuzhao1027@gmail.com>
2019-12-08 22:26:19 -0500

    Add log.

--- src/main/java/com/example/demo/rest/BillingCycleController.java
+++ src/main/java/com/example/demo/rest/BillingCycleController.java
@@ -3,13 +3,18 @@ package com.example.demo.rest;
 import com.example.demo.domain.outbound.Outpayload;
 import com.example.demo.domain.inbound.Inpayload;
 import com.example.demo.service.BillingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.invoke.MethodHandles;
 
 @RestController
 @RequestMapping("/billingcycle")
 public class BillingCycleController {
 
+    private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     @Autowired
     BillingService billingService;
 
@@ -21,6 +26,7 @@ public class BillingCycleController {
         if(inpayload == null) {
             return new Outpayload();
         }
+        log.info("format payload");
         return billingService.processPayload(inpayload);
     }
 }
